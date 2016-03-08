8bdd2e40389524de48130eaef3b23fd5bc9678a2
MohmadAyman <no.future2e1@gmail.com>
2016-03-08 18:38:51 +0200

    yassin last edit

--- scanner.c
+++ scanner.c
@@ -5,82 +5,48 @@ extern int yylex();
 extern int yylineno;
 extern char* yytext;
 
-char* names[] = { "KEYWORD", "ID", "COLN" , "OP" ,"SEMCOLN" , "NUMBER" , "CHAR" , "LPR"};
+char* names[] = { "ERROR", "IF", "ElSE" 
+, "FOR" ,"SWITCH" , "CASE" 
+, "WHILE" , "RETURN" ,"UNTIL" , 
+ "CONST" , "ID" , "COLN",
+ "SEMCOLN" , "FLOATING" , "CHARACTER" ,
+ "LPR","RPR", "LBRA", 
+ "RBRA","ASSN","COMA",
+ "WHITESPACE","PLUS","MINUS",
+ "DIV","MULT","MOD",
+ "GREATER", "LESS","EQUALEQUAL"
+ ,"GREATEREQUAL", "LESSEQUAL","NOTEQUAL",
+ "NOT","AND","XOR" 
+ ,"OR", "XNOR","SHIFTL",
+ "SHIFTR", "PRINT","BREAK"
+ ,"CONTINUE","LABEL", "GOTO",
+ "STRINGVAL","LSQUAREPR","RSQUAREPR"
+ ,"DO","DEFAULT","INTEGER"
+ ,"INT","FLOAT", "CHAR"
+ ,"VOID","STRING","INCREMENT",
+ "DECREMENT" ,"BITOR","BITXOR" ,
+ "BITXNOR","BITAND","BITNOT",
+ "LINECOMMENT","PRACOMMENT"};
+ 
 
 int main(void)
-{
-	int ntoken, vtoken;
+{	int ntoken, vtoken;
 	ntoken = yylex();
 	while(ntoken){
-		printf("%d\n", ntoken);
-		ntoken = yylex();
-		if(yylex()==ERROR){
+		if(ntoken==ERROR){
 			printf("syntax error in line %d, found %s\n ",
 		yylineno, yytext );
 		return 1;
-		            }
-		vtoken = yylex();
-                 printf("vtoken  %d ", vtoken); 
-		switch(vtoken)
-			{
-			case KEYWORD:
-				printf("KEYWORD :D %s\n", names[ntoken]);
-				break;
-			case ID:
-				printf("Identifier\n");
-				break;
-			case COLN:
-				printf("Colon");
-				break;
-			case OP:
-				printf("operator");
-				break;
-			case SEMCOLN:
-				printf("Semi Colon ");
-				break;
-			case NUMBER:
-				printf("num ");
-				break;
-			case CHAR:
-				printf("character ");
-				break;
-			case LPR:
-				printf("left prases ");
-				break;
-			case RPR:
-				printf("right prases ");
-				break;
-			case LBRA:
-				printf("left bracket ");
-				break;
-			case RBRA:
-				printf("right bracket");
-				break;
-			case COMP:
-				printf("comparison");
-				break;
-			case ASSGN:
-				printf("assign ");
-				break;
-			case COMA:
-				printf("coma ");
-				break;
-			case WHITESPACE:
-				printf("whitespace ");
-				break;
-			case LOGICAL:
-				printf("logical");
-				break;
-              
-		if(vtoken == ERROR){
-                  printf("syntax error in line %d, found %s \n , expected %d",yylineno, yytext , vtoken);
-				}
-		//else 	printf("%s is set to %s\n", names[ntoken],
-		// yytext);	
-		
-		return 1;
 		}
+		if(ntoken == WHITESPACE)
+		{printf("%d :  %s\n", ntoken, names[ntoken-1]);}
+		else printf("%d :  %s   value = %s\n", ntoken, names[ntoken-1], yytext );
+        ntoken = yylex();		
+		//vtoken = yylex();
 
-	}
+				 
+		}
+    printf("%d :  %s\n", ntoken, names[ntoken-1]);    
+	
     return 0;
 }
